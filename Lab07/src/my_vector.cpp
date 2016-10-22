f275800b3aaf3d0e1613bc0b1a2d44590d0b4b2f
aliscafo <ero.alina@gmail.com>
2016-10-22 18:12:15 +0300

    MyVector

---
+++ Lab07/src/my_vector.cpp
@@ -0,0 +1,85 @@
+#include "../include/my_vector.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iostream>
+#include <stdio.h>
+
+MyVector::MyVector() {
+  _cp = 2;
+  _sz = 0;
+  _data = new int[2];
+}
+
+MyVector::MyVector(std::size_t init_capacity) {
+  _cp = init_capacity;
+  _sz = 0;
+  _data = new int[init_capacity];
+}
+
+MyVector::~MyVector() {
+  delete []_data;
+}
+
+void MyVector::set(std::size_t index, int value) {
+  assert(0 <= index && index < _sz);
+  _data[index] = value;
+}
+
+int MyVector::get(std::size_t index) {
+  assert(0 <= index && index < _sz);
+  return _data[index];
+}
+
+std::size_t MyVector::size() {
+  return _sz;
+}
+
+std::size_t MyVector::capacity() {
+  return _cp;
+}
+
+void MyVector::reserve(std::size_t new_capacity) {
+  if (_cp < new_capacity) {
+    _data = (int*)realloc(_data, new_capacity * sizeof(int));
+    _cp = new_capacity;
+  }  
+}
+
+void MyVector::resize(std::size_t new_size) {
+  if (_cp < new_size) {
+    reserve(new_size);
+  }
+
+  for (size_t i = _sz; i < new_size; i++)
+    _data[i] = 0; 
+
+  _sz = new_size;
+}
+
+void MyVector::push_back(int value) {
+  if (_sz + 1 > _cp)
+    reserve(_cp * 2);  
+  _sz++;
+  set(_sz - 1, value);
+}
+
+void MyVector::insert(std::size_t index, int value) {
+  _sz++;
+  assert(0 <= index && index < _sz);
+  reserve(_sz);
+  for (size_t i = _sz - 1; i > index; i--)
+    _data[i] = _data[i - 1];
+  set(index, value);
+}
+
+void MyVector::erase(std::size_t index) {
+  assert(0 <= index && index < _sz);
+
+  for (size_t i = index; i < _sz - 1; i++)
+    _data[i] = _data[i + 1];
+  _sz--;    
+}
+
+
+
